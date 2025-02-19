229177b770629f5c29a28840d74b8c2370bf1d8c
maria <fesychmarimia@gmail.com>
2025-02-19 17:39:03 +0100

    start

---
+++ src/js/1-timer.js
@@ -0,0 +1,65 @@
+// Описаний в документації
+import flatpickr from 'flatpickr';
+// Додатковий імпорт стилів
+import 'flatpickr/dist/flatpickr.min.css';
+const startBtn = document.querySelector('[data-start]');
+const days = document.querySelector('[data-days]');
+const hours = document.querySelector('[data-hours]');
+const minutes = document.querySelector('[data-minutes]');
+const seconds = document.querySelector('[data-seconds]');
+
+let userSelectedDate;
+
+startBtn.addEventListener('click', onClick);
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const startTime = Date.now();
+    if (selectedDates[0] <= startTime) {
+      alert('Please choose a date in the future');
+      startBtn.setAttribute('disabled', 'true');
+    } else {
+      startBtn.removeAttribute('disabled');
+      userSelectedDate = selectedDates[0];
+    }
+  },
+};
+
+flatpickr('#datetime-picker', options);
+
+function onClick() {
+  setInterval(() => {
+    const timerTime = userSelectedDate.getTime() - Date.now();
+    const obj = convertMs(timerTime);
+    days.innerHTML = obj.days;
+    hours.innerHTML = obj.hours;
+    minutes.innerHTML = obj.minutes;
+    seconds.innerHTML = obj.seconds;
+  }, 1000);
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
